7a4f4b7e91cfb8a1675f6bb541c49282d6c55757
Kenneth69875 <kennetha2409@hotmail.com>
2025-09-19 09:17:14 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const titulo = document.getElementById("titulo");
+const btnColor = document.getElementById("btnColor");
+
+btnColor.addEventListener("click", () => {
+    titulo.style.color = titulo.style.color === "white" ? "black" : "white";
+});
